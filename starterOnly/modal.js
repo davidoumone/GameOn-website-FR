dca84dba8aaa3e2f24832096fcd838799a6fa1c7
Davidoumone <david.moneta28@gmail.com>
2022-01-04 15:11:08 +0100

    formulaire terminé

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -22,41 +22,64 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.classList.add("bground-apparition");
-  modalbg.classList.remove("bground");
+  modalbg.classList.add("bground-apparition"); // ajoute la classe avec le display block
+  modalbg.classList.remove("bground"); // retire la classe avec le display none
 }
 
 // modal form close
 modalclose.onclick = function () {
-  modalbg.classList.remove("bground-apparition");
-  modalbg.classList.add("bground");
+  modalbg.classList.remove("bground-apparition"); // retire la classe avec le display block
+  modalbg.classList.add("bground"); // ajoute la classe avec le display none
 };
 
 // validation du formulaire
 function validate() {
-  modalConfirm.classList.add("modalConfirmation-apparition");
-  modalConfirm.classList.remove("modalConfirmation");
-  modalbg.classList.remove("bground-apparition");
-  modalbg.classList.add("bground");
+  modalbg.classList.remove("bground-apparition"); // retire la classe avec le display block
+  modalbg.classList.add("bground"); // ajoute la classe avec le display none
+  modalConfirm.classList.remove("modalConfirmation"); // retire la classe avec le display none
+  modalConfirm.classList.add("modalConfirmation-apparition"); // ajoute la classe avec le display block
+}
+
+// réinitialise le formulaire
+function initialize() {
+  form.first.value = null;
+  form.last.value = null;
+  form.email.value = null;
+  form.birthdate.value = null;
+  form.quantity.value = null;
+  // reinitialsation des locations
+  for (let index = 0; index < localisation.length; index++) {
+    localisation[index].checked = "";
+  }
+  // reinitialisation des messages d'erreurs
+  let myerreur = document.getElementsByClassName("erreur");
+  for (let msgerreur = 0; msgerreur < myerreur.length; msgerreur++) {
+    myerreur[msgerreur].classList.add("erreur-disappared");
+  }
 }
 
 // modalConfirmation close
 Confirmclose.onclick = function () {
-  modalConfirm.classList.remove("modalConfirmation-apparition");
-  modalConfirm.classList.add("modalConfirmation");
+  modalConfirm.classList.remove("modalConfirmation-apparition"); // retire la classe avec le display block
+  modalConfirm.classList.add("modalConfirmation"); // ajoute la classe avec le display none
+  // appel de la fonction initialize
+  initialize();
 };
 
 // form submit
 form.addEventListener("submit", function (event) {
   /*****************SOUMISSION DU FORMULAIRE **********************/
   event.preventDefault();
-  if (
-    validfirst(form.first) ||
-    validlast(form.last) ||
-    validemail(form.email) ||
-    validbirthdate(form.birthdate) ||
-    validquantite(form.quantity)
-  ) {
+  // champ prénom
+  if (validfirst(form.first)) {
+  } else {
+    modalConfirm.classList.remove("modalConfirmation-apparition");
+    modalConfirm.classList.add("modalConfirmation");
+    modalbg.classList.add("bground-apparition");
+    modalbg.classList.remove("bground");
+  }
+  // champ nom
+  if (validlast(form.last)) {
   } else {
     modalConfirm.classList.remove("modalConfirmation-apparition");
     modalConfirm.classList.add("modalConfirmation");
@@ -64,14 +87,41 @@ form.addEventListener("submit", function (event) {
     modalbg.classList.remove("bground");
   }
 
-  let localisationValid = false;
-  let i = 0;
-
-  while (!localisationValid && i < localisation.length) {
-    if (localisation[i].checked) localisationValid = true;
-    i++;
+  // champ email
+  if (validemail(form.email)) {
+  } else {
+    modalConfirm.classList.remove("modalConfirmation-apparition");
+    modalConfirm.classList.add("modalConfirmation");
+    modalbg.classList.add("bground-apparition");
+    modalbg.classList.remove("bground");
+  }
+  // champ birthdate
+  if (validbirthdate(form.birthdate)) {
+  } else {
+    modalConfirm.classList.remove("modalConfirmation-apparition");
+    modalConfirm.classList.add("modalConfirmation");
+    modalbg.classList.add("bground-apparition");
+    modalbg.classList.remove("bground");
+  }
+  // champ quantite
+  if (validquantite(form.quantity)) {
+  } else {
+    modalConfirm.classList.remove("modalConfirmation-apparition");
+    modalConfirm.classList.add("modalConfirmation");
+    modalbg.classList.add("bground-apparition");
+    modalbg.classList.remove("bground");
   }
 
+  // champ localisation
+  let localisationValid = false;
+  let i = 0;
+  // tant que la condition localisationValid est vrai et i inférieur a localisation
+  while (!localisationValid && i < localisation.length) {
+    // La propriété checked définit ou renvoie l'état coché d'une case à cocher.
+    if (localisation[i].checked) localisationValid = true; // la variable passe a vrai
+    i++;
+  }
+  // si une destination n'est pas sélectionné
   if (!localisationValid) {
     let myerreur = document.getElementsByClassName("erreur")[5];
     myerreur.innerHTML = "Veuillez selectionner une destination";
@@ -83,6 +133,8 @@ form.addEventListener("submit", function (event) {
     modalbg.classList.remove("bground");
   }
 
+  // champ checkbox
+  // si les conditions générales d'utilisation ne sont pas cochés
   if (!document.getElementById("checkbox1").checked) {
     let myerreur = document.getElementsByClassName("erreur")[6];
     myerreur.innerHTML = "Vous devez accepté les contitions d'utilisations";
@@ -96,7 +148,9 @@ form.addEventListener("submit", function (event) {
 });
 
 /***************** CHAMP PRENOM ********************************/
+// addEventListener() méthode pour enregistrer un écouteur d'événement avec change comme type d'évènements.
 form.first.addEventListener("change", function () {
+  // appel de la fonction prise en charge this = paramètre de la fonction validfirst (firstname)
   validfirst(this);
 });
 
